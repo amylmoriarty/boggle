a5df23f8c27cc8950f1d44337ad434d8125c4d35
Amy Moriarty <lostjrt16@gmail.com>
2018-09-19 23:52:09 +0000

    Added fourth test - all tests passing

--- test_boggle.py
+++ test_boggle.py
@@ -23,3 +23,16 @@ class TestBoggle (unittest.TestCase):
         grid = boggle.make_grid (2, 3)
         self.assertEqual(len(grid), 6)
         
+        
+    def test_grid_coordinates (self):
+        """
+        Test to ensure that all the coordinates
+        inside of the grid can be accessed
+        """
+        grid = boggle.make_grid (2, 2)
+        self.assertIn((0, 0), grid)
+        self.assertIn((0, 1), grid)
+        self.assertIn((1, 0), grid)
+        self.assertIn((1, 1), grid)
+        self.assertNotIn((2, 2), grid)
+        
