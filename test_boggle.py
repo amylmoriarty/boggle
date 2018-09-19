01d5b386a074f7751bb7460bdd12969c7a352d63
Amy Moriarty <lostjrt16@gmail.com>
2018-09-19 23:15:15 +0000

    Second Test - failed

--- test_boggle.py
+++ test_boggle.py
@@ -1,6 +1,17 @@
 import unittest
+import boggle
 
 
-class test_boggle (unittest.TestCase):
-    def test_is_this_thing_on (self):
-        self.assertEqual(1, 1)
+class TestBoggle (unittest.TestCase):
+    """
+    Our Test Suite for Boggle Solver
+ 
+    """
+    def test_can_create_an_empty_grid (self):
+    """
+    Test to see if we can create an empty grid
+    """
+    grid = boggle.make_grid (0, 0)
+    self.assertEqual(len (grid), 0 )
+    
+    
