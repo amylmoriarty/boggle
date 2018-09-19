faf28306595eb8eacb5dc01102f21413bd98c941
Amy Moriarty <lostjrt16@gmail.com>
2018-09-19 23:21:18 +0000

    Second Test Passed

--- test_boggle.py
+++ test_boggle.py
@@ -8,10 +8,10 @@ class TestBoggle (unittest.TestCase):
  
     """
     def test_can_create_an_empty_grid (self):
-    """
-    Test to see if we can create an empty grid
-    """
-    grid = boggle.make_grid (0, 0)
-    self.assertEqual(len (grid), 0 )
-    
+        """
+        Test to see if we can create an empty grid
+        """
+        grid = boggle.make_grid (0, 0)
+        self.assertEqual(len (grid), 0 )
+        
     
